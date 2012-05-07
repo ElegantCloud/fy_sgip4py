3e629b18fb8a2e67c927bdf79cd8822a019728bf
starkingwx <starkingwx@gmail.com>
2012-05-07 18:49:36 +0800

    don't close the socket after handling deliver message'

--- src/feiying/sgip_server.py
+++ src/feiying/sgip_server.py
@@ -62,7 +62,6 @@ class SGIPProcessor(object):
                 self.__handle_bind_msg(header) 
             elif header.CommandID == SGIPDeliver.ID:
                 self.__handle_deliver_msg(header) 
-                break
             elif header.CommandID == SGIPUnbind.ID:
                 self.__send_sgip_unbind_resp(header)
                 break
