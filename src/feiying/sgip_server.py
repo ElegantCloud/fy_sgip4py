046773325e18662ced9797992c5578f77ca435b3
starkingwx <starkingwx@gmail.com>
2012-05-08 17:53:05 +0800

    change RESPONSE text message info

--- src/feiying/sgip_server.py
+++ src/feiying/sgip_server.py
@@ -22,10 +22,10 @@ db_name = 'feiying'
 SMG_ADDRS = ('220.195.192.85', '127.0.0.1')
 
 # respone messages
-DZFY_OK = '尊敬的用户，您已成功订购飞影业务，请进入http://fy1.richitec.com/feiying/mobile下载客户端'
-TDFY_OK = '尊敬的用户，您已成功退订飞影业务，感谢您的使用。'
-DZFY_FAIL = '尊敬的用户，您已订购过飞影业务，请勿重复订购。'
-TDFY_FAIL = '尊敬的用户，您尚未开通飞影业务，无法执行退订操作。'
+DZFY_OK = '尊敬的客户：您已成功订购飞影业务，请进入http://fy1.richitec.com/feiying/mobile下载客户端'
+TDFY_OK = '尊敬的客户：您已成功退订飞影业务，感谢您的使用。'
+DZFY_FAIL = '尊敬的客户：您已订购过飞影业务，请勿重复订购。'
+TDFY_FAIL = '尊敬的客户：您尚未开通飞影业务，无法执行退订操作。'
 
 # SGIP Message Processor
 class SGIPProcessor(object):
